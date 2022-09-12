e979766e2def54c16dd47cb56bfa768c6dfe3c50
Jillian Salloum <salloum1@msu.edu>
2022-09-12 18:06:08 -0400

    finished part 1

--- starter/03check-errors/checkErrorsTest.test.ts
+++ starter/03check-errors/checkErrorsTest.test.ts
@@ -3,16 +3,32 @@ import {add} from './checkErrors'
 
 it('TODO: task 3 goes here', () => {
     // Arrange
-  
+  let emptyArr = []
+
     // Act
+    function errCheck(emptyArr){
+  if (emptyArr.length === 0) {
+    let emptyArrError = 'The array is empty'
+    return emptyArrError
   
     // Assert
-  })
-  
+    //Implement new assertion → it should throw an error if an empty array is provided. Test it by running your test command. Commit if passing
+    expect(errCheck).toThrow(emptyArrError)
+  }
+  }
+})
+
   it('TODO: task 4 goes here', () => {
     // Arrange and Act
     // create a function that calls the add function without any arguments and assign it to a variable
-  
-    // Assert
+
+    function add2(param){
+      if (param === 0) {
+        let emptyAddError = 'There is nothing to add'
+        return emptyAddError
+        
+        // Assert
     // use the expect function to assert that the function throws an error, use toThrow function
+  expect(add2).toThrowError(emptyAddError)
+
   })
--- starter/04more-assertions/util/numbers.test.ts
+++ starter/04more-assertions/util/numbers.test.ts
@@ -5,17 +5,18 @@ it('should transform a string number to a number of type number', () => {
 
     const result = transformToNumber(input)
 
-    expect(result).toBe('number')
+    expect(result).not.toBeNaN()
 })
 
 it('TODO: Task 3 goes here', () => {
     // Arrange
     // create a variable with a string value that is a valid number, e.g. '1'
-
+let three = '3'
     // Act
     // call the transformToNumber function on an input value and assign the result to a variable
-
+let result = transformToNumber(three)
     // Assert
+    expect(result).toBe(3)
     // use the expect function to assert that the result is equal to the expected result
 })
 
@@ -26,7 +27,10 @@ it('TODO: Task 4 goes here', () => {
 
     // Act
     // for each input value, call the transformToNumber function on an input value and assign the result to a variable
-
+let firstInput = transformToNumber(input)
+let secondInput = transformToNumber(input2)
     // Assert
+    expect(firstInput).toBeNaN()
+    expect(secondInput).toBeNaN()
     // for each result value, use the expect function to assert that the result is equal to NaN, use toBeNaN function
 })
