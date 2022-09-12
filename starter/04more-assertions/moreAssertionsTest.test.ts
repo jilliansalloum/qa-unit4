2694359560a80f36b7b38dcd3748ad1eadb12ee6
Jillian Salloum <salloum1@msu.edu>
2022-09-12 18:14:09 -0400

    ok i mean now

--- starter/04more-assertions/moreAssertionsTest.test.ts
+++ starter/04more-assertions/moreAssertionsTest.test.ts
@@ -11,14 +11,14 @@ it('TODO: task 3 goes here', () => {
 it('should throw an error if no value is passed into the function', () => {
   // Arrange and Act
   // create a function that calls the add function without any arguments and assign it to a variable
-  function add2(param){
+  function add(param){
     if (param === 0) {
       let emptyAddError = 'There is nothing to add'
       return emptyAddError
       
       // Assert
   // use the expect function to assert that the function throws an error, use toThrow function
-  expect(add2).toThrowError(emptyAddError)
+  expect(add).toThrowError(emptyAddError)
     }
   }
   })
